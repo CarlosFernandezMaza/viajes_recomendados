ddbd6885fb329b9e8f76bc1fb91265c06e054e97
Carlos Fernández Maza <carlos@onlinevalles.com>
2023-05-10 21:30:19 +0200

    commit del dia 10

--- src/routes/comentariesRoutes.js
+++ src/routes/comentariesRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const validateAuth = require("../middlewares/validateAuth");
-const { createComentarie, createComentarieByIdTrip } = require("../controllers.js/comentaries/comentariesController");
+const { createComentarieByIdTrip } = require("../controllers.js/comentaries/comentariesController");
 const comentariesRoutes = express.Router();
 
 //endpoints privados
-comentariesRoutes.route("/:id/comentaries").all(/*validateAuth*/).post(createComentarieByIdTrip);
+comentariesRoutes.route("/:id/comentaries").all(validateAuth).post(createComentarieByIdTrip);
 
 module.exports = {
     comentariesRoutes
